314a5deb7b6ebd1c32eaa00adcb7fe69f2d765c6
Daniel Hebenstreit <111197814+Daniel-Hebenstreit@users.noreply.github.com>
2025-03-24 12:14:01 +0100

    removed build folder

--- build/Desktop_Qt_6_8_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appMapIntegration_FILE /home/hebi/QML Tut/Other/MapIntegration/build/Desktop_Qt_6_8_1-Debug/appMapIntegration)
-set(__QT_DEPLOY_TARGET_appMapIntegration_TYPE EXECUTABLE)
--- build/Desktop_Qt_6_8_1-Debug/qtcsettings.cmake
+++
@@ -1,4 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_PREFIX_PATH" "/home/hebi/QtInstall/6.8.2/gcc_64;/home/hebi/QtInstall/6.8.1/gcc_64" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/home/hebi/QtInstall/6.8.2/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
